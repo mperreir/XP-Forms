146d2e830de4f2721de38e361765b650bab264a1
Mohamed Jaouada <E23A156G>
2025-03-04 10:19:23 +0100

    Ajout d'un README qui decrit l'application et comment l'executer

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-rou
 import React from "react";
 import './App.css';
 import Accueil from "./pages/accueil/accueil";
-import FormEditor from "./pages/form_editor/form_editor";
 import FormEditor2 from "./pages/form_editorr/form_editorr";
 import FormViewer from "./pages/form_viewer/form_viewer";
 
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route index element={<Accueil/>}></Route>
           <Route path="/accueil" element={<Accueil/>}></Route>
-          <Route path="/form-editor" element={<FormEditor />} />
           <Route path="/form-editor2" element={<FormEditor2 />} />
           <Route path="/form-viewer/:id" element={<FormViewer />} />
         </Routes>
