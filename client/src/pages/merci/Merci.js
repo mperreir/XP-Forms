d39136266b1a7198b902d1bc054938a1b0c67321
Julien CHATRY <chatryjulien@gmail.com>
2025-08-21 20:17:51 +0200

    [UPDATE] Style de la page Merci

--- client/src/pages/merci/Merci.js
+++ client/src/pages/merci/Merci.js
@@ -1,8 +1,22 @@
 import React from "react";
+import styles from "./Merci.css"
 
 const Merci = () => {
+  const body = document.getElementsByTagName('body')[0]; 
+  body.style.zoom = '1'; // Remove zoom to center body properly
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div 
+      id="merci"
+      style={
+        { textAlign: "center", 
+        height: "100vh", 
+        display: "flex", 
+        flexDirection: "column", 
+        justifyContent: "center", 
+        alignItems: "center" }
+        }
+    >
       <h1>Merci pour votre réponse !</h1>
       <p>Veuillez appuyer sur la touche F10 pour passer manuellement au stimulus suivant.</p>
     </div>
