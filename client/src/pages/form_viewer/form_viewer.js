eec76fcf024f7741d3c3de5127efce420ef5b0e3
Mathis Gillaizeau <mathis.gillaizeau@etu.univ-nantes.fr>
2026-01-12 09:29:58 +0100

    fix warning

--- client/src/pages/form_viewer/form_viewer.js
+++ client/src/pages/form_viewer/form_viewer.js
@@ -374,7 +374,7 @@ const validateCurrentPage = useCallback(() => {
       formRef.current = null;
       form.destroy();
     };
-  }, [schema, pages, currentPage, componentMapping, id_participant, navigate, fetchSavedData, id]);
+  }, [schema, pages, currentPage, componentMapping, id_participant, navigate, fetchSavedData, id, effectiveCurrentPage, effectivePages]);
 
   // Recalculer la validité de la page courante à chaque changement de formData
   useEffect(() => {
@@ -389,7 +389,7 @@ const validateCurrentPage = useCallback(() => {
     }, 100);
     
     return () => clearTimeout(timeoutId);
-  }, [formData, currentPage, schema, pages, validateCurrentPage]);
+  }, [formData, currentPage, schema, pages, validateCurrentPage, effectivePages.length]);
 
   return (
     <>
